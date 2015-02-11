01af78198b74ef46f3982e5369bc3aa88102d70f
Marcus Kaseder <marcus.kaseder@online.de>
2015-02-11 16:47:43 +0100

    Added Theme and Custom CSS support

--- API/Controllers/DesignController.cs
+++ API/Controllers/DesignController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Security;
+using System.Web.Configuration;
 using System.Web.Hosting;
 using System.Web.Http;
 using API.Models;
@@ -32,23 +33,114 @@ namespace API.Controllers
                 if (site.IsOffice365)
                 {
                     //only relevant if SharePoint Online
-                    var username = "dev@mkamsdn.onmicrosoft.com";
-                    var password = "Start111!".ToSecureString();
+                    var username = WebConfigurationManager.AppSettings["UserName"];
+                    var password = WebConfigurationManager.AppSettings["Password"].ToSecureString();
                     var credentials = new SharePointOnlineCredentials(username, password);
                     clientContext.Credentials = credentials;
                 }
 
-                clientContext.Load(clientContext.Web);
-                clientContext.Load(clientContext.Web, w => w.ServerRelativeUrl);
+                Web web = clientContext.Web;
+                // Let's first upload the contoso theme to host web, if it does not exist there
+                //web.DeployThemeToWeb("SPC",
+                //                HostingEnvironment.MapPath(string.Format("~/{0}", "Resources/Themes/SPC/SPCTheme.spcolor")),
+                //                null,
+                //                HostingEnvironment.MapPath(string.Format("~/{0}", "Resources/Themes/SPC/SPCbg.jpg")),
+                //                string.Empty);
+
+                string themeDisplayName = "IPI Theme";
+                string themeName = "ipi_theme";
+                bool hasBackgroundImage = false;
+                bool hasFonts = false;
+
+                string filePath = HostingEnvironment.MapPath(string.Format("~/assets/themes/{0}.spcolor", themeName));
+                web.UploadThemeFile(filePath);
+
+                filePath = HostingEnvironment.MapPath(string.Format("~/assets/themes/{0}.png", themeName));
+                if (System.IO.File.Exists(filePath))
+                {
+                    hasBackgroundImage = true;
+                    web.UploadThemeFile(filePath);
+                }
+
+                filePath = HostingEnvironment.MapPath(string.Format("~/assets/themes/{0}.spfont", themeName));
+                if (System.IO.File.Exists(filePath))
+                {
+                    hasFonts = true;
+                    web.UploadThemeFile(filePath);
+                }
+
+                web.CreateComposedLookByName(
+                    themeDisplayName,
+                    string.Format("{0}.spcolor", themeName),
+                    hasFonts ? string.Format("{0}.spfont", themeName) : null,
+                    hasBackgroundImage ? string.Format("{0}.png", themeName) : null,
+                    null);
+
+                // Setting the Contoos theme to host web
+                //web.SetThemeToWeb("SPC");
+                web.SetComposedLookByUrl(themeDisplayName, resetSubsitesToInherit: true);
+                //web.SetComposedLookByUrl(themeDisplayName, resetSubsitesToInherit: true, rootWeb: clientContext.Site.RootWeb);
+
+                //clientContext.Load(clientContext.Web);
+                //clientContext.Load(clientContext.Web, w => w.ServerRelativeUrl);
+                //clientContext.ExecuteQuery();
+
+                //clientContext.Web.DeployMasterPage(HostingEnvironment.MapPath("~/assets/masterpage/customMaster.master"), "Custom Master Page", "");
+                //clientContext.Web.RootFolder.UploadFile("alternate.css", HostingEnvironment.MapPath("~/assets/css/alternate.css"), true);
+
+                //string masterpageUrl = string.Format("{0}/_catalogs/masterpage/customMaster.master", clientContext.Web.ServerRelativeUrl);
+                //clientContext.Web.SetMasterPagesByUrl(masterpageUrl, masterpageUrl);
+                //clientContext.Web.AlternateCssUrl = string.Format("", clientContext.Web.ServerRelativeUrl);
+
+                //clientContext.ExecuteQuery();
+
+
+
+                ///////// CUSTOM CSS //////////              
+                filePath = HostingEnvironment.MapPath(string.Format("~/assets/css/{0}.css", themeName));
+                List assetLibrary = web.GetListByUrl("SiteAssets");
+                assetLibrary.RootFolder.UploadFileWebDav(System.IO.Path.GetFileName(filePath), filePath, true);
+
+                //// Get the path to the file which we are about to deploy
+                //string file = System.Web.Hosting.HostingEnvironment.MapPath(string.Format("~/{0}", "CSS/contoso.css"));
+
+                //// Use CSOM to uplaod the file in
+                //FileCreationInformation newFile = new FileCreationInformation();
+                //newFile.Content = System.IO.File.ReadAllBytes(file);
+                //newFile.Url = "contoso.css";
+                //newFile.Overwrite = true;
+                //Microsoft.SharePoint.Client.File uploadFile = assetLibrary.RootFolder.Files.Add(newFile);
+                //clientContext.Load(uploadFile);
+                //clientContext.ExecuteQuery();
+
+                // Now, apply a reference to the CSS URL via a custom action
+                string actionName = string.Format("{0}CSSLink", themeName);
+
+                // Clean up existing actions that we may have deployed
+                var existingActions = web.UserCustomActions;
+                clientContext.Load(existingActions);
+
+                // Execute our uploads and initialize the existingActions collection
                 clientContext.ExecuteQuery();
 
-                clientContext.Web.DeployMasterPage(HostingEnvironment.MapPath("~/assets/masterpage/customMaster.master"), "Custom Master Page", "");
-                clientContext.Web.RootFolder.UploadFile("alternate.css", HostingEnvironment.MapPath("~/assets/css/alternate.css"), true);
+                var actions = existingActions.ToArray();
+                // Clean up existing custom action with same name, if it exists
+                foreach (var existingAction in actions)
+                {
+                    if (existingAction.Name.Equals(actionName, StringComparison.InvariantCultureIgnoreCase))
+                        existingAction.DeleteObject();
+                }
+                clientContext.ExecuteQuery();
 
-                string masterpageUrl = string.Format("{0}/_catalogs/masterpage/customMaster.master", clientContext.Web.ServerRelativeUrl);
-                clientContext.Web.SetMasterPagesByUrl(masterpageUrl, masterpageUrl);
-                clientContext.Web.AlternateCssUrl = string.Format("", clientContext.Web.ServerRelativeUrl);
+                // Build a custom action to write a link to our new CSS file
+                UserCustomAction cssAction = web.UserCustomActions.Add();
+                cssAction.Location = "ScriptLink";
+                cssAction.Sequence = 100;
+                cssAction.ScriptBlock = string.Format("document.write('<link rel=\"stylesheet\" href=\"{0}/{1}\" />');", assetLibrary.RootFolder.ServerRelativeUrl, System.IO.Path.GetFileName(filePath));
+                cssAction.Name = actionName;
 
+                // Apply
+                cssAction.Update();
                 clientContext.ExecuteQuery();
             }
 
